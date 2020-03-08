e63806424de1fc38a23da08d70a98a7c82fe7cf6
hiratsukashu <hiratsukashu@indival.co.jp>
2020-03-08 23:47:24 +0900

    detail

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { Text, AppRegistry, View } from 'react-native';
+// In App.js in a new project
 
-// 追加
-import Header from './common/Header';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// 修正
-const App = () => (
-  <View>
-    <Header showText={'Hello'} />
-  </View>
-);
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Hiratsuka Test" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
