298dd7cc391f2b10099f86f647f1fdd5b2d063ed
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2020-04-10 23:47:54 +0900

    リストを修正

--- worklist.js
+++ worklist.js
@@ -9,156 +9,177 @@ import {
   FlatList,
 } from 'react-native';
 import Constants from 'expo-constants';
+import ImagePlaceholder from 'react-native-image-with-placeholder';
 import axios from 'axios';
 
 
-  function Item({ workitem }) {
-    console.log(workitem);
-    return (
-      <View style={styles.item}>
-        <Text style={styles.title}>{workitem.CatchCopy}</Text>
+function Item({ workitem }) {
+  console.log(workitem);
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{workitem.CatchCopy}</Text>
         <View style={{ flexDirection: 'row'}}>
-          <Text style={styles.itemTitle}>給与情報</Text>
-          <Text style={styles.itemTitle}>{workitem.Payment}</Text>
-        </View>
-        <View style={{ flexDirection: 'row'}}>
-          <Text style={styles.itemTitle}>職種</Text>
-          <Text style={styles.itemTitle}>{workitem.JobName}</Text>
-        </View>
-        <View style={{ flexDirection: 'row'}}>
-          <Text style={styles.itemTitle}>駅</Text>
-          <Text style={styles.itemTitle}>{workitem.WorkPlace}</Text>
-        </View>
-        <View style={{ flexDirection: 'row'}}>
-          <Text style={styles.itemTitle}>時間</Text>
-          <Text style={styles.itemTitle}>{workitem.WorkDateTime}</Text>
-        </View>
+          <BaseWorkInfo workitem={workitem} />
+          <ImageFavorite workitem={workitem} />
+        </ View>
+      
+    </View>
+  );
+}
+
+function ImageFavorite({ workitem }){
+  return (
+    <View>
+    </View> 
+  );
+
+}
+
+
+function BaseWorkInfo({ workitem }){
+  return (
+    <View>
+      <View style={{ flexDirection: 'row'}}>
+        <Text style={styles.itemTitle}>給与情報</Text>
+        <Text style={styles.itemTitle}>{workitem.Payment}</Text>
       </View>
-    );
+      <View style={{ flexDirection: 'row'}}>
+        <Text style={styles.itemTitle}>職種</Text>
+        <Text style={styles.itemTitle}>{workitem.JobName}</Text>
+      </View>
+      <View style={{ flexDirection: 'row'}}>
+        <Text style={styles.itemTitle}>駅</Text>
+        <Text style={styles.itemTitle}>{workitem.WorkPlace}</Text>
+      </View>
+      <View style={{ flexDirection: 'row'}}>
+        <Text style={styles.itemTitle}>時間</Text>
+        <Text style={styles.itemTitle}>{workitem.WorkDateTime}</Text>
+      </View>
+    </View>
+  );
+}
+
+class WorkList extends Component {
+  
+  currentOffset = 0;
+
+  state = {
+    worklists: null,
+    listMeta: null,
+    loading: false,
+    isRefreshing: false
+  }
+
+  componentDidMount() {
+      
+    this.fetchWorkList();
   }
   
-  class WorkList extends Component {
-    
-    currentOffset = 0;
 
-    state = {
-      worklists: null,
-      listMeta: null,
-      loading: false,
-      isRefreshing: false
+  render() {
+    if (this.state.loading && this.page === 1) {
+      return <View style={{
+        width: '100%',
+        height: '100%'
+      }}><ActivityIndicator style={{ color: '#000' }} /></View>;
     }
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={this.state.worklists}
+          renderItem={({ item }) => <Item workitem={item} />}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.isRefreshing}
+              onRefresh={this.onRefresh.bind(this)}
+            />
+          }
+          keyExtractor={item => item.WorkId}
+          ListFooterComponent={this.renderFooter.bind(this)}
+          onEndReachedThreshold={0.4}
+          onEndReached={this.handleLoadMore.bind(this)}
+        />
+      </SafeAreaView>
+    )
+  }
 
-    componentDidMount() {
-       
-      this.fetchWorkList();
-    }
-   
-
-    render() {
-      if (this.state.loading && this.page === 1) {
-        return <View style={{
-          width: '100%',
-          height: '100%'
-        }}><ActivityIndicator style={{ color: '#000' }} /></View>;
-      }
+  renderFooter = () => {
+    //it will show indicator at the bottom of the list when data is loading otherwise it returns null
+      if (!this.state.loading) return null;
       return (
-        <SafeAreaView style={styles.container}>
-          <FlatList
-            data={this.state.worklists}
-            renderItem={({ item }) => <Item workitem={item} />}
-            refreshControl={
-              <RefreshControl
-                refreshing={this.state.isRefreshing}
-                onRefresh={this.onRefresh.bind(this)}
-              />
-            }
-            keyExtractor={item => item.WorkId}
-            ListFooterComponent={this.renderFooter.bind(this)}
-            onEndReachedThreshold={0.4}
-            onEndReached={this.handleLoadMore.bind(this)}
-          />
-        </SafeAreaView>
-      )
+        <ActivityIndicator
+          style={{ color: '#000' }}
+        />
+      );
+  };
+
+    handleLoadMore = () => {
+    if (!this.state.loading) {
+      this.setState({ loading: true });
+      this.currentOffset = this.currentOffset + 20; // increase page by 1
+      this.fetchWorkList(); // method for API call 
     }
+  };
 
-    renderFooter = () => {
-      //it will show indicator at the bottom of the list when data is loading otherwise it returns null
-       if (!this.state.loading) return null;
-       return (
-         <ActivityIndicator
-           style={{ color: '#000' }}
-         />
-       );
-    };
-
-     handleLoadMore = () => {
-      if (!this.state.loading) {
-        this.setState({ loading: true });
-        this.currentOffset = this.currentOffset + 20; // increase page by 1
-        this.fetchWorkList(); // method for API call 
-      }
-    };
-
-    onRefresh() {
-      this.currentOffset = 1
-      this.setState({ isRefreshing: true }); // true isRefreshing flag for enable pull to refresh indicator
-      this.fetchWorkList()
-    }
-
-    constructor(props) {
-      super(props);
-      this.state = {date: new Date()};
-      console.log("constructor");
-    }
-
-    fetchWorkList(){
-      var param = {'a':'01','pr':'13','start':this.currentOffset,'limit':'20'}
-  
-      axios
-      .get('https://api-front.shotworks.jp/api-front/app/worklist', { params: param　})
-      .then((results) => {
-          // 通信に成功してレスポンスが返ってきた時に実行したい処理
-          this.setState({ worklists: results.data.Result, listMeta: results.data.ResultSet, isRefreshing: false, loading: false });
-      })
-      .catch((error) => { 
-          // 通信に失敗してレスポンスが返ってこなかった時に実行したい処理
-          console.log('処理に失敗しました');
-          this.setState({ worklists: null, listMeta: null, isRefreshing: false, isloading: false });
-      });     
-    
-    }
-
-    
+  onRefresh() {
+    this.currentOffset = 1
+    this.setState({ isRefreshing: true }); // true isRefreshing flag for enable pull to refresh indicator
+    this.fetchWorkList()
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+    console.log("constructor");
+  }
+
+  fetchWorkList(){
+    var param = {'a':'01','pr':'13','start':this.currentOffset,'limit':'20'}
+
+    axios
+    .get('https://api-front.shotworks.jp/api-front/app/worklist', { params: param　})
+    .then((results) => {
+        // 通信に成功してレスポンスが返ってきた時に実行したい処理
+        this.setState({ worklists: results.data.Result, listMeta: results.data.ResultSet, isRefreshing: false, loading: false });
+    })
+    .catch((error) => { 
+        // 通信に失敗してレスポンスが返ってこなかった時に実行したい処理
+        console.log('処理に失敗しました');
+        this.setState({ worklists: null, listMeta: null, isRefreshing: false, isloading: false });
+    });     
+  
+  }
+
+  
+}
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: Constants.statusBarHeight,
-      marginHorizontal: 10,
-    },
-    item: {
-      backgroundColor: '#ffffff',
-      padding: 20,
-      marginVertical: 8,
-    },
-    header: {
-      fontSize: 28,
-    },
-    title: {
-      fontSize: 13,
-    },
-    title: {
-      fontSize: 13,
-    },
-    itemTitle: {
-      fontSize: 10,
-      color: '#444444',
-      marginVertical: 5,
-      marginHorizontal: 2,
-    },
-  });
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 10,
+  },
+  item: {
+    backgroundColor: '#ffffff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 28,
+  },
+  title: {
+    fontSize: 13,
+  },
+  title: {
+    fontSize: 13,
+  },
+  itemTitle: {
+    fontSize: 10,
+    color: '#444444',
+    marginVertical: 5,
+    marginHorizontal: 2,
+  },
+});
 
 export default WorkList;  
-  
